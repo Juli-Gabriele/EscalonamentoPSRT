1a39c1a3e11ebe40cd22cc99374291d6ee83590d
karine-vitoriia <karinevitoriasilvarodrigues@gmail.com>
2026-04-07 14:12:58 -0300

    declaração de variaveis p usar nesse algor.

--- src/Processo.java
+++ src/Processo.java
@@ -6,6 +6,8 @@ class Processo {
     String nome;
     int tempoExecucao;
     int tempoChegada;
+    int tempoRestante; // p controlar quanto falta para acabar na cpu
+    int prioridade;    // p usar no algorit de priorid.
 
     public Processo(String nome, int tempoChegada, int tempoExecucao) {
         this.nome = nome;
